5b2f56b04c2c693d70ab3cb297e1fa41bcb31fdf
Amin Haddadi <amin.haddadi1995@gmail.com>
2025-07-23 14:45:55 +0330

    feature : add filter to products list page

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,26 +1,49 @@
+"use client";
 import { products } from "@/lib/mock";
+import { Product } from "@/types";
 import Link from "next/link";
+import { useState } from "react";
 
 const Products = () => {
+  const [search, setSearch] = useState("");
+
+  const filteredProducts = products.filter((product: Product) =>
+    product.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="max-w-2xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-6 text-center">لیست محصولات</h1>
-      <ul className="space-y-4">
-        {products.map((product) => (
-          <li
-            key={product.id}
-            className="p-4 border rounded-lg shadow-sm hover:shadow-md hover:scale-110 transition duration-500"
-          >
+    <div className="max-w-4xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6 text-center">محصولات</h1>
+
+      <input
+        type="text"
+        placeholder="جستجوی نام محصول..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="w-full px-4 py-2 mb-6 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        autoFocus
+      />
+
+      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-6">
+        {filteredProducts.map((product) => (
+          <li key={product.id}>
             <Link href={`/Products/${product.id}`}>
-              <div className="cursor-pointer flex justify-between items-center">
-                <h2 className="text-xl font-semibold">{product.name}</h2>
-                <p className="text-sky-700">
+              <div className="p-4 bg-white rounded-lg shadow hover:shadow-lg transition hover:scale-110 duration-500 cursor-pointer h-full">
+                <h2 className="text-xl font-bold text-gray-500 mb-1">
+                  {product.name}
+                </h2>
+                <p className="text-sky-800">
                   {product.price.toLocaleString()} تومان
                 </p>
               </div>
             </Link>
           </li>
         ))}
+        {filteredProducts.length === 0 && (
+          <p className="text-center text-gray-500 col-span-full">
+            هیچ محصولی یافت نشد.
+          </p>
+        )}
       </ul>
     </div>
   );
