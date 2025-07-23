d09eb83d5c486649ccc7890d02c20546a93cdf2d
Amin Haddadi <amin.haddadi1995@gmail.com>
2025-07-23 14:08:13 +0330

    refactor : add product types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,16 @@ export type Todo = {
   id: string;
   title: string;
 };
+
+export type Product = {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+};
+
+export type ProductProps = {
+  params: {
+    id: string;
+  };
+};
